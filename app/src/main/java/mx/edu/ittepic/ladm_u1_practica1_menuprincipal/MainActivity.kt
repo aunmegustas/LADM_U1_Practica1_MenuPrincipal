190c3d2ae565838edfff9940274b5c24768a00c8
aunmegustas <cralperezhe@ittepic.edu.mx>
2022-09-12 18:10:12 -0600

    Se agregó un nuevo EmptyActivity para el botón Imagen, y la funcionalidad de dicho botón y el botón música y la creación de los métodos onClickListener de los 4 botones.

--- app/src/main/java/mx/edu/ittepic/ladm_u1_practica1_menuprincipal/MainActivity.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u1_practica1_menuprincipal/MainActivity.kt
@@ -1,12 +1,51 @@
 package mx.edu.ittepic.ladm_u1_practica1_menuprincipal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import mx.edu.ittepic.ladm_u1_practica1_menuprincipal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imagen.setOnClickListener {
+            var imagen = Intent(this, Imagen::class.java)
+            startActivity(imagen)
+        }
+        binding.musica.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("Apartado de Música")
+                .setPositiveButton("Aceptar"){
+                    d,i->
+                    d.dismiss()
+                }
+                .setNegativeButton("Cancelar"){
+                    d,i->
+                    d.cancel()
+                    Toast.makeText(this,"Qué tratas de cancelar? No hace nada esta alerta de dialogo.",Toast.LENGTH_LONG)
+                        .show()
+                }
+                .setNeutralButton("Salir"){
+                    d,i->
+                    finish()
+                }
+                .show()
+
+        }
+        binding.gif.setOnClickListener {
+
+        }
+        binding.animacion.setOnClickListener {
+
+        }
+
     }
+
 }
